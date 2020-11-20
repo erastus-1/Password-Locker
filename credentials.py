2e90f0df86689f31122e015af706854ce7f912b7
erastus <erastuskariuki15@gmail.com>
2020-11-20 21:37:19 +0300

    add input and functionality

--- credentials.py
+++ credentials.py
@@ -3,7 +3,7 @@ class Credentials:
     Class that generates new instances of credentials.
     '''
      
-    credential_list =[] #empty credential list
+    credentials_list =[] #empty credential list
 
     def __init__(self,first_name,second_name,account,username,password):
 
@@ -13,3 +13,53 @@ class Credentials:
        self.username = username
        self.password = password
 
+    
+    def save_credentials(self):
+        '''
+        save_credentials method saves objects to credentials list
+        '''
+        Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        '''
+        delete_credentials method deletes objects to credentials list
+        '''
+        Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def find_by_first_name(cls,first_name):
+        '''
+        Method that takes in first name and returns a user that matches that first name.
+        
+        Args:
+             credentials: First_name to search for a credential
+        Returns:
+             Account of the user that matches the name
+        '''
+
+        for credentials in cls.credentials_list:
+            if credentials.first_name == first_name:
+                return credentials
+
+    @classmethod
+    def credentials_exist(cls,first_name):
+        '''
+        Method that checks if a contact exists from the user list.
+        Args:
+            credentials: first name to search if it exists
+        Returns :
+            Boolean: True or false depending if the credential exists
+        '''
+        for Credentials in cls.credentials_list:
+            if Credentials.first_name == first_name:
+                    return True
+
+        return False
+
+    @classmethod
+    def display_credentials(cls):
+        '''
+        display_credentials is a method that dispalys all the available credential accounts
+        '''
+        return cls.credentials_list
+            
